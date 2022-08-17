bb7e519e5901e3c7b9c2ab03287a40610b90e14c
youngseok Kim <komorebi930@gmail.com>
2022-08-17 11:35:18 +0900

    11:35 Commit!

--- src/main/java/aug12/Test2.java
+++ src/main/java/aug12/Test2.java
@@ -7,7 +7,10 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 class A {A(){ 
-	System.out.println("A");}}
+	System.out.println("A");
+	int r = (int)(Math.random() *10 + 1);
+	}
+}
 class B extends A { 
 	B(){ 
 		super();//super 생성자
@@ -22,7 +25,7 @@ class C extends B {
 	}
 class D extends C { 
 	D(){ 
-			super();//super 생성자 : 부모의 생성자를 호출하는 것
+			super();//super 생성자 : 부모의 생성자를 호출하는 것 // 입력 안 할 시 자바 컴파일러가 자동으로 삽입
 			System.out.println("D");
 		}
 	}
@@ -37,6 +40,7 @@ public class Test2 extends HttpServlet {
 
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		D d = new D(); //class를 데이터로 바꾸는 것 instance
+		//D의 조상이 전부 instance로 바뀌어야 함
 	}
 
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
---
+++ src/main/java/aug17/Page6.java
@@ -0,0 +1,91 @@
+package aug17;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+class IamKing {
+	String name;
+	IamKing(String name){
+		this.name = name;
+	}
+}
+class IamSon extends IamKing {
+	IamSon(){
+		super("È«±æµ¿");// ----> IamKing("È«±æµ¿");
+	}
+}
+class One {
+	int i;
+	String name;
+	One(int i) {
+		this.i = i;
+	}
+	One(String name) {
+		this.name = name;
+	}
+}
+class Two extends One {
+	Two(int i) {
+		super(i);
+	}
+	Two(String name){
+		super(name);
+	}
+}
+
+class Hana {
+	int i;
+	int j;
+	double k;
+	Hana(int i, int j) {
+		this.i = i;
+		this.j = j;
+	}
+	Hana(double k) {
+		this.k = k;
+	}
+}
+class Dool extends Hana {
+	Dool(int i, int j){
+		super(i, j);
+	}
+	Dool(double k){
+		super(k);
+	}
+}
+class Seet extends Dool {
+	Seet(int i, int j){
+		super(i, j);
+	}
+	Seet(double k){
+		super(k);
+	}
+}
+
+@WebServlet("/Page6")
+public class Page6 extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+    public Page6() {
+        super();
+    }
+
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		Seet seet1 = new Seet(10, 11);
+		Seet seet2 = new Seet(3.14);
+		Two tw1 = new Two(5);
+		Two tw2 = new Two("±è±æµ¿");
+		IamSon s1 = new IamSon();
+		IamKing k1 = new IamKing("¾È³ç");
+		IamKing k2 = new IamKing("korea");
+		IamKing k3 = new IamKing("È«±æµ¿");
+	}
+
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+	}
+
+}
